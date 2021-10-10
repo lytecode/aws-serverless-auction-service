5c17f4e634ec2be58d21eec40869866b04e1d21e
Mbonu Basil <mbonubasil@gmail.com>
2021-10-10 14:26:49 +0100

    Add create aution function

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,7 +1,18 @@
 async function createAuction(event, context) {
+  const { title, description } = JSON.stringify(event.body);
+  const date = new Date();
+
+  const auction = {
+    title,
+    description,
+    status: "OPEN",
+    createAt: date.toISOString(),
+    modifiedAt: date.toISOString(),
+  };
+
   return {
-    statusCode: 200,
-    body: JSON.stringify({ event, context }),
+    statusCode: 201,
+    body: JSON.stringify({ auction }),
   };
 }
 
