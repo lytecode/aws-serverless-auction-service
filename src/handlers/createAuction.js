a3af9d897ca0063bab7da796b2695969e53657e6
Mbonu Basil <mbonubasil@gmail.com>
2021-10-12 21:33:39 +0100

    Modify response body on success

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,8 +1,16 @@
+import { v4 as uuid } from "uuid";
+import AWS from "aws-sdk";
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
 async function createAuction(event, context) {
+  const LOG_KEY = "CREATE-AUCTION";
+
   const { title, description } = JSON.stringify(event.body);
   const date = new Date();
 
   const auction = {
+    id: uuid(),
     title,
     description,
     status: "OPEN",
@@ -10,9 +18,24 @@ async function createAuction(event, context) {
     modifiedAt: date.toISOString(),
   };
 
+  try {
+    await dynamodb
+      .put({
+        TableName: "AuctionsTable",
+        Item: auction,
+      })
+      .promise();
+  } catch (error) {
+    console.log(LOG_KEY, error);
+    return {
+      statusCode: 500,
+      body: { error: "Could not create auction" },
+    };
+  }
+
   return {
     statusCode: 201,
-    body: JSON.stringify({ auction }),
+    body: JSON.stringify(auction),
   };
 }
 
