677347b59d9d47eee4eebcd1becb4ac0453e0ed6
Mbonu Basil <mbonubasil@gmail.com>
2021-10-13 21:09:53 +0100

    Add endpoint to get Auction by Id

---
+++ src/handlers/getAuction.js
@@ -0,0 +1,37 @@
+import AWS from "aws-sdk";
+import commonMiddleware from "../lib/commonMiddleware";
+import createError from "http-errors";
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function getAuction(event, context) {
+  const { id } = event.pathParameters;
+  let auction;
+
+  try {
+    const result = await dynamodb
+      .get({
+        TableName: process.env.AUCTIONS_TABLE_NAME,
+        Key: { id },
+      })
+      .promise();
+
+    auction = result.Item;
+  } catch (error) {
+    console.log(error);
+    throw new createError.InternalServerError(
+      "Request failed, try again later"
+    );
+  }
+
+  if (!auction) {
+    throw new createError.NotFound(`Auction with id ${id} not found`);
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(auction),
+  };
+}
+
+export const handler = commonMiddleware(getAuction);
